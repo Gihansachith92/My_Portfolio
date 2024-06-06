f3eb371003983e89d817d9c941840f10a2c9f84c
Gihan_Sachith <gihansachith92@gmail.com>
2024-06-06 23:56:39 +0900

    js codes

---
+++ main.js
@@ -0,0 +1,31 @@
+/********************  Toggle Icon Navbar *****************************/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+   menuIcon.classList.toggle('fa-xmark');
+   navbar.classList.toggle('active');
+}
+
+
+/********************  scroll section active link *****************************/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top => offset && top < offset + height){
+      navLinks.forEach.apply(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+
+  });
+}
