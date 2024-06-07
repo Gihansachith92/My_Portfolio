03420b4e2048c7e613d1257c111892be001ee77f
Gihan_Sachith <gihansachith92@gmail.com>
2024-06-07 23:54:34 +0900

    code

--- main.js
+++ main.js
@@ -28,4 +28,37 @@ window.onscroll = () => {
     };
 
   });
+
+ /********************  stiky navbar *****************************/
+ let header = document.querySelector('header');
+ header.classList.toggle('sticky' ,window.scrollY > 100 );
+
+ /********************  remove toggle icon and navbar *****************************/
+ menuIcon.classList.remove('fa-xmark');
+ navbar.classList.remove('active');
+
+
 }
+
+ /********************  scroll reveal *****************************/
+ ScrollReveal({ 
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+
+  });
+
+
+  ScrollReveal().reveal('.home-content, heading', { origin:'top' });
+  ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'buttom' });
+  ScrollReveal().reveal('.about-img', { origin:'left' });
+  ScrollReveal().reveal(' .about-content', { origin:'right' });
+
+ /********************  typed js *****************************/
+const typed = new Typed('.multiple-text', {
+  strings: ['Frontend Developer', 'Web Designer', 'Java Developer'],
+  typeSpeed: 30,
+  backSpeed: 40,
+  backDelay: 800,
+  loop: true,
+});
